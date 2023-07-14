92474ea22f5d2adfd64567421ca9a69b29f74145
Jamela <JAMELA MARITHE BAIRA>
2023-07-14 13:41:39 +0800

    Added firestore

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,13 +1,15 @@
 import { GoogleAuthProvider } from '@angular/fire/auth';
 import { Injectable } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/compat/auth';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
+import { Observable } from '@firebase/util';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor( private afs: AngularFireAuth ) { }
+  constructor( private afs: AngularFireAuth, private firestore: AngularFirestore) { }
 
   signInWithGoogle() {
     return this.afs.signInWithPopup(new GoogleAuthProvider());
@@ -20,5 +22,4 @@ export class AuthService {
   signInWithEmailAndPassword(user: {email: string, password: string}) {
     return this.afs.signInWithEmailAndPassword(user.email, user.password);
   }
-
 }
